c7d9b337dda7509f6840f67d94d17d9a7c4c1ea6
Alim Ismael Kamara <92651821+akamara2002@users.noreply.github.com>
2023-02-12 00:28:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const countDownDate = new Date("Mar 3, 2023 00:00:00").getTime();
+const clock = document.querySelector(".clock");
+
+
+// Update the count down every 1 second
+const interval = setInterval(function () {
+
+    // Get today's date and time
+    const now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    const distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    clock.innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(interval);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
