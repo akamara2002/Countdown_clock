b2b2620157a6b4eff87728bfe3d176ec66b70fc0
Alim Ismael Kamara <92651821+akamara2002@users.noreply.github.com>
2024-06-22 14:54:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const countDownDate = new Date("Nov 3, 2024 00:00:00").getTime();
 const clock = document.querySelector(".clock");
 
-
 // Update the count down every 1 second
 const interval = setInterval(function () {
 
@@ -17,13 +16,12 @@ const interval = setInterval(function () {
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Output the result in an element with id="demo"
-    clock.innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
+    // Output the result in an element with class="clock"
+    clock.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
     // If the count down is over, write some text 
     if (distance < 0) {
         clearInterval(interval);
-        document.getElementById("demo").innerHTML = "EXPIRED";
+        clock.innerHTML = "EXPIRED";
     }
 }, 1000);
